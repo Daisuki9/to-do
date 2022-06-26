f5225aed149861c17b66ad712adf880b0b676795
Daisuki9 <daiana_zab@hotmail.com>
2022-06-25 22:44:17 -0300

    agregado de on_delete en foreignKeys

--- todo/apptodo/models.py
+++ todo/apptodo/models.py
@@ -8,12 +8,12 @@ class CategoriaDeEstado(models.Model):
 
 class Estado(models.Model):
     Titulo=models.CharField(max_length=30)
-    Categoria=models.ForeignKey(CategoriaDeEstado)
+    Categoria=models.ForeignKey(CategoriaDeEstado, on_delete=models.CASCADE)
 
 class Tarea(models.Model):
     Titulo=models.CharField(max_length=50)
     Fecha=models.DateField()
     Contenido=models.TextField(max_length=8000)
     Completado=models.BooleanField()
-    Estado=models.ForeignKey(Estado)
-    Proyecto=models.ForeignKey(Proyecto)
+    Estado=models.ForeignKey(Estado, on_delete=models.CASCADE)
+    Proyecto=models.ForeignKey(Proyecto, on_delete=models.CASCADE)
