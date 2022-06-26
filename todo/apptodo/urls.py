c09dd2a389316287d7e021ef8a96de4fd33976b3
Daisuki9 <daiana_zab@hotmail.com>
2022-06-26 03:11:58 -0300

    plantilla base sin usar

--- todo/apptodo/urls.py
+++ todo/apptodo/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('', Inicio, name="inicio"),
+    path('base/', Base),
     path('proyectos/', Proyectos, name="proyectos"),
     path('estados/', Estados, name="estados"),
     #path('tareas/', Tareas, name="tareas"),
--- todo/apptodo/views.py
+++ todo/apptodo/views.py
@@ -3,21 +3,22 @@ from django.http import HttpResponse
 
 from .models import *
 
+def Base(request):
+    proyectos=Proyecto.objects.all()
+    return render(request, "base.html", {})
+
 def Inicio(request):
     proyectos=Proyecto.objects.all()
     return render(request, "index.html", {})
 
-def Proyectos(request):
-    
+def Proyectos(request):    
     proyectos=Proyecto.objects.all()
     return render(request, "proyectos.html", {"proyectos":proyectos})
     
-def Estados(request):
-    
+def Estados(request):    
     return render(request, "estados.html", {})
 
 def Tareas(request, claveProyecto=''):
     proyectos=Proyecto.objects.all()
     tareas=Tarea.objects.all()
-
     return render(request, "tareas.html", {"proyectos":proyectos, "tareas":tareas})
